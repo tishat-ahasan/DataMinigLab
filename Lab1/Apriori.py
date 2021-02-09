71750be54856d9274474e68aea32e9182157afe4
tishat-ahasan <tishat.ahasan004@gmail.com>
2021-02-09 13:19:52 +0600

    Everything of Lab 1

---
+++ Lab1/Apriori.py
@@ -0,0 +1,179 @@
+import math
+import resource
+import tracemalloc
+import time
+
+class TrieNode:
+    def __init__(self, item):
+        self.item = item
+        self.is_end = False
+        self.counter = 0
+        self.children = {}
+        
+class Trie(object):
+
+    def __init__(self):
+        self.root = TrieNode("root")
+        
+    def initial_insert(self,item_list,MIN_COUNT):
+        for key in sorted(item_list):
+            if item_list[key] < MIN_COUNT:  # non frequent item
+                continue
+            new_node = TrieNode(key)
+            new_node.counter = item_list[key]
+            new_node.is_end = True
+            self.root.children[str(key)] = new_node
+    
+    def insert(self, item_set):
+        node = self.root
+        for item in item_set:
+            if item in node.children:
+                node = node.children[item]
+            else:
+                new_node = TrieNode(item)
+                node.children[item] = new_node
+                node = new_node
+        node.is_end=True
+    
+    def generate_candidate(self,root): # Last node ta ki kora jay!!..............
+        if root.is_end: return
+        keys = list(root.children.keys())
+        for i in range (len(keys)):
+            cur_node = root.children[keys[i]] 
+            if cur_node.is_end == True:
+                for j in range(i+1,len(keys)):
+                    new_node = TrieNode(root.children[keys[j]].item)
+                    new_node.is_end = True
+                    cur_node.is_end = False
+                    cur_node.children[keys[j]] = new_node
+            else:
+                self.generate_candidate(cur_node)
+    
+    def print_trie(self,root,level):
+        if root.is_end == True:
+            print(level,"leave -->", root.item)
+            print(level,"count:",root.counter)
+            return
+        else:
+            print(level,"childer of------>",root.item)
+            print(level,"count:",root.counter)
+            level = level+"  "+level
+            for child in root.children.values():
+                self.print_trie(child,level)
+                
+    def frequency_count(self,root,transaction_list,left):
+        if root.is_end == True:
+            root.counter += 1
+            return
+        for child in root.children.values():
+            if child.item in transaction_list[left:]:
+                offset = transaction_list[left:].index(child.item)
+#                 self.frequency_count(child,transaction_list,left)
+                self.frequency_count(child,transaction_list,left+offset)
+        return
+    
+    def return_count(self,root,candidate,support_count):
+        if root.is_end == True:
+            if root.item!= 'root': 
+                support_count[candidate] = root.counter
+                #print(candidate,":",root.counter)
+#             pri#nt(root.item)
+            return
+        if (len(candidate)!=0): candidate += ","
+        for child in root.children.values():
+            abc = self.return_count(child,candidate+str(child.item),support_count)
+
+                
+    def remove_nonfrequent_item(self,root,min_item,level,cur_level):
+        if (root.is_end):
+            if (cur_level < level or root.counter < min_item): return True
+            else: return False
+        delete = [key for key,value in root.children.items() if (self.remove_nonfrequent_item(value,min_item,level,cur_level+1))]
+        for key in delete: 
+            del root.children[key]
+        if (len(root.children)==0): 
+            root.is_end = True
+            return True
+        return False
+    
+    def database_scan(self,transactions):
+        for transaction in transactions:
+            self.frequency_count(self.root,transaction,0)
+
+
+def get_data(filename):
+    file = open(filename, 'r')
+    data = []
+    for line in file:
+        row = line.strip().split(' ')
+        row = [int(x) for x in row]
+        row.sort()
+        data.append(row)
+        
+        
+    file.close()
+    return data
+def item_count(transactions):
+    item_list = {}
+    for transaction in transactions:
+        for item in transaction:
+            # key = int(str(item))
+            key = item
+            item_list[key] = item_list.get(key, 0) + 1
+    return item_list
+
+def run(item_list,transactions,MIN_COUNT,trie,FREQUENT_PATTERN):
+    batch_no = 1
+    while True:
+        if batch_no==1:
+           trie.initial_insert(item_list,MIN_COUNT) 
+          #  trie.print_trie(trie.root,"")
+        else:
+            trie.generate_candidate(trie.root)
+    #         trie.remove_nonfrequent_item(trie.root,0,batch_no,0)
+            trie.database_scan(transactions)
+            #print("............................")
+            #trie.return_count(trie.root,"",FREQUENT_PATTERN) 
+            #trie.return_count()
+            trie.remove_nonfrequent_item(trie.root,MIN_COUNT,batch_no,0)
+        if trie.root.is_end == True: break
+        
+        trie.return_count(trie.root,"",FREQUENT_PATTERN) 
+        batch_no += 1
+    return
+
+def Apriori(fname , min_support):
+    transactions = get_data(fname)
+    item_list = item_count(transactions)
+    total_transactions = len(transactions)
+    MIN_COUNT = math.ceil(total_transactions*min_support) 
+    FREQUENT_PATTERN = {}
+    trie = Trie()
+    run(item_list,transactions,MIN_COUNT,trie,FREQUENT_PATTERN)
+    return FREQUENT_PATTERN
+
+def main():
+    print("Generating patterns using Apriori....")
+    fname = 'tr.txt'
+    tracemalloc.start()
+    S_Time = time.process_time()
+    min_sup = 0.4
+    fp = Apriori(fname,min_sup)
+
+    current, peak = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+    E_TIME = round(time.process_time()-S_Time,4)
+    MEMORY = round(peak/10**6,4)
+    dash = '-' * 90
+    print(dash)
+    print('{:<25s}{:>8s}{:>18s}{:>16s}{:>17s}'.format('Dataset', 'min_sup', 'patterns', 'Runtime' , 'Memory'))
+    print(dash)
+    print('{:<25s}{:>6.2f}{:>16d}{:>18.3f}{:>18.3f}'.format(fname,min_sup*100,len(fp),E_TIME,MEMORY))
+    # print(dash)
+    for key,value in fp.items():
+    	print(key,"\t",value)
+    
+
+
+if __name__ == '__main__':
+    main()
---
+++ Lab1/FPGrowth.py
@@ -0,0 +1,158 @@
+from collections import defaultdict, OrderedDict
+import pandas as pd
+from itertools import chain, combinations
+from optparse import OptionParser
+import operator
+import resource
+import tracemalloc
+import time
+
+
+class Node:
+    def __init__(self, itemName, frequency, parentNode):
+        self.itemName = itemName
+        self.count = frequency
+        self.parent = parentNode
+        self.children = {}
+        self.next = None
+
+    def display(self, ind=1):
+        print('  ' * ind, self.itemName, ' ', self.count)
+        for child in list(self.children.values()):
+            child.display(ind+1)
+
+def constructTree(transactions, frequency, minSup):
+    item_set = defaultdict(int)
+    # Counting frequency and create header table
+    for idx, transaction in enumerate(transactions):
+        for item in transaction:
+            item_set[item] += frequency[idx]
+
+    
+    frequent_items = {}
+    for key,value in item_set.items():
+        if value >= minSup:
+            frequent_items[key] = value
+    del(item_set)
+
+    frequent_items = dict( sorted(frequent_items.items(), key=operator.itemgetter(1),reverse=True))
+
+    headerTable = defaultdict(int)
+    for key,value in frequent_items.items():
+#         print("key:",key,"value:",value)
+        headerTable[key] = [value, None]
+    del(frequent_items)
+    
+    # Init Null head node
+    fpTree = Node('Null', 1, None)
+    # Update FP tree for each cleaned and sorted itemSet
+    for idx, itemSet in enumerate(transactions):
+        itemSet = list(filter(lambda v: v in headerTable, itemSet))
+#         itemSet = [item for item in itemSet if item in headerTable]
+        itemSet.sort(key=lambda item: headerTable[item][0], reverse=True)
+        # Traverse from root to leaf, update tree with given item
+        currentNode = fpTree
+        for item in itemSet:
+            currentNode = updateTree(item, currentNode, headerTable, frequency[idx])
+    return fpTree, headerTable
+
+def updateTree(item, treeNode, headerTable, frequency):
+    if item in treeNode.children:
+        # If the item already exists, increment the count
+        treeNode.children[item].count += frequency
+    else:
+        # Create a new branch
+        newItemNode = Node(item, frequency, treeNode) #item,frequency,parent_node
+        treeNode.children[item] = newItemNode
+        # Link the new branch to header table
+        updateHeaderTable(item, newItemNode, headerTable)
+    return treeNode.children[item]
+
+def updateHeaderTable(item, targetNode, headerTable):
+    if(headerTable[item][1] == None):
+        headerTable[item][1] = targetNode
+    else:
+        currentNode = headerTable[item][1]
+        # Traverse to the last node then link it to the target
+        while currentNode.next != None:
+            currentNode = currentNode.next
+        currentNode.next = targetNode
+
+
+def mineTree(headerTable, minSup, preFix, freqItemList):
+    for key,value in headerTable.items():  
+        # Pattern growth is achieved by the concatenation of suffix pattern with frequent patterns generated from conditional FP-tree
+        if preFix == "": newPattern = preFix + key
+        else: newPattern = preFix + "," + key
+        freqItemList[newPattern] += value[0]
+        # Find all prefix path, constrcut conditional pattern base
+        conditionalPattBase, frequency = findPrefixPath(key, headerTable) 
+        # Construct conditonal FP Tree with conditional pattern base
+        conditionalTree, newHeaderTable = constructTree(conditionalPattBase, frequency, minSup) 
+        if newHeaderTable != None:
+            # Mining recursively on the tree
+            mineTree(newHeaderTable, minSup,
+                       newPattern, freqItemList)
+
+def findPrefixPath(basePat, headerTable):
+    # First node in linked list
+    treeNode = headerTable[basePat][1] 
+    condPats = []
+    frequency = []
+    while treeNode != None:
+        prefixPath = []
+        # From leaf node all the way to root
+        climbFPTree(treeNode, prefixPath)  
+        if len(prefixPath) > 1:
+            # Storing the prefix path and it's corresponding count
+            condPats.append(prefixPath[1:])
+            frequency.append(treeNode.count)
+        # Go to next node
+        treeNode = treeNode.next    
+    return condPats, frequency
+
+def climbFPTree(node, prefixPath):
+    if node.parent != None:
+        prefixPath.append(node.itemName)
+        climbFPTree(node.parent, prefixPath)
+
+def fpgrowth(fname, minSupport):
+    transactions, frequency = get_data(fname)
+    MIN_COUNT = len(transactions) * minSupport
+    fpTree, headerTable = constructTree(transactions, frequency, MIN_COUNT)
+    del(transactions)
+    freqItems = defaultdict(int)
+    mineTree(headerTable, MIN_COUNT, "", freqItems)
+    return freqItems
+def get_data(filename):
+    transactions = []
+    frequency = []
+    
+    file = open(filename, 'r')
+    for line in file:
+        row = line.strip().split(' ')
+        transactions.append(row)
+        frequency.append(1)
+    file.close()
+    return transactions, frequency
+
+def main():
+    fname = 'transactions.txt'
+    tracemalloc.start()
+    S_Time = time.process_time()
+    min_sup = 0.2
+    fp = fpgrowth(fname,min_sup)
+    current, peak = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
+    E_TIME = round(time.process_time()-S_Time,4)
+    MEMORY = round(peak/10**6,4)
+
+    dash = '-' * 80
+    print(dash)
+    print('{:<20s}{:>8s}{:>18s}{:>16s}{:>17s}'.format('fname', 'min_sup', 'patterns', 'Runtime' , 'Memory'))
+    print(dash)
+    print('{:<20s}{:>6.2f}{:>16d}{:>18.3f}{:>18.3f}'.format(fname,min_sup*100,len(fp),E_TIME,MEMORY))
+    
+
+if __name__ == '__main__':
+    main()
